ff643ebba1b3879fb62568050ded3b43d299f547
hcysm1 <soniamubasher1991@gmail.com>
2025-05-15 08:18:15 -0400

    fixed linting

--- tests/factories.py
+++ tests/factories.py
@@ -31,8 +31,6 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
-
     name = FuzzyChoice(
         choices=[
             "Hat",
